bd9e34db1462ba87f3bacf73ef6e8fafe3fa5f35
Gualter <gualtermacedo@hotmail.com>
2026-07-20 19:48:13 +0000

    feat: mapeamentos Odisseias dinâmicos — DB + Resource Filament + acção inline + fix Tratamentos de Corpo

---
+++ app/Filament/Resources/OdisseiasServiceMappings/Pages/CreateOdisseiasServiceMapping.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Filament\Resources\OdisseiasServiceMappings\Pages;
+
+use App\Filament\Resources\OdisseiasServiceMappings\OdisseiasServiceMappingResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateOdisseiasServiceMapping extends CreateRecord
+{
+    protected static string $resource = OdisseiasServiceMappingResource::class;
+}
