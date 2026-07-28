51058a09a70771f7e2a7a5f7359ca73292b9cb16
Gualter Macedo <gualtermacedo@hotmail.com>
2026-07-28 14:42:00 +0000

    fix: $view não-static no PackSessoesWidget (Filament v4)

--- app/Filament/Widgets/PackSessoesWidget.php
+++ app/Filament/Widgets/PackSessoesWidget.php
@@ -7,7 +7,7 @@ use Filament\Widgets\Widget;
 
 class PackSessoesWidget extends Widget
 {
-    protected static string $view = 'filament.widgets.pack-sessoes';
+    protected string $view = 'filament.widgets.pack-sessoes';
     protected int|string|array $columnSpan = 'full';
     protected static ?int $sort = 10;
 
